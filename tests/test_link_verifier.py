48947dc4edb02db06d7241d3e6428b2b09e0e2d0
Megan Folsom <mfolsom@gmail.com>
2025-01-28 14:17:13 -0800

    Get tests to pass

--- tests/test_link_verifier.py
+++ tests/test_link_verifier.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 @pytest.fixture
 def link_verifier():
-    links = ["https://example.com", "https://invalid-link.com"]
+    links = ["https://example.com", "https://invalid-link.com","https://www.starbucks.com/madeup"]
     return LinkVerifier(links)
 
 def test_check_links_present(link_verifier):
@@ -16,9 +16,16 @@ def test_check_links_present(link_verifier):
 @patch('requests.head')
 def test_verify_link_targets(mock_head, link_verifier):
     mock_head.return_value.status_code = 200
-    link_status = link_verifier.verify_link_targets()
+    link_status, final_urls, unreachable_links = link_verifier.verify_link_targets()
     assert link_status["https://example.com"] == 'valid', "Link should be marked as valid"
 
 def test_verify_link_targets_invalid(link_verifier):
-    link_status = link_verifier.verify_link_targets()
+    link_status, final_urls, unreachable_links = link_verifier.verify_link_targets()
+    print(f"LINKSTATUS***{link_status}")
+    assert link_status["https://www.starbucks.com/madeup"] == 'invalid', "Link should be marked as invalid"
+
+def test_verify_link_targets_unreachable(link_verifier):
+    link_status, final_urls, unreachable_links = link_verifier.verify_link_targets()
     assert link_status["https://invalid-link.com"] == 'unreachable', "Link should be marked as unreachable"
+
+
